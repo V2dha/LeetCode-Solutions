37de9c867366e0bc27d28eaf28d8abda52461f1e
Vividha <rvividha@gmail.com>
2021-01-23 22:07:37 +0530

    Time: 100 ms (5.05%), Space: 14.2 MB (60.55%) - LeetHub

--- roman-to-integer/roman-to-integer.py
+++ roman-to-integer/roman-to-integer.py
@@ -1,22 +1,30 @@
 class Solution:
     def romanToInt(self, s: str) -> int:
-        intValue = 0
-        romanLen = len(s) - 1
-        i = 0
-        values = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        RomanDict = {
+            'I' : 1,
+            'V' : 5,
+            'X' : 10,
+            'L' : 50,
+            'C' : 100,
+            'D' : 500,
+            'M' : 1000
+        }
         
-        while i < romanLen:
-            currentInt = values[s[i]]
-            nextInt = values[s[i+1]]
-            
-            if currentInt in (1, 10, 100) and nextInt in (currentInt*5, currentInt*10):
-                intValue += nextInt - currentInt
-                i += 2
+        total = 0
+        i=0
+        while i < len(s)-1:
+            curr_ele = RomanDict[s[i]]
+            next_ele = RomanDict[s[i+1]]
+            if curr_ele >= next_ele:
+                total+=curr_ele
+                i+=1
             else:
-                intValue += currentInt
-                i += 1
+                total+=(next_ele-curr_ele)
+                i+=2
+            
+        if i==len(s)-1:
+            total+=RomanDict[s[i]]
+        return total
                 
-        if i == romanLen:
-            intValue += values[s[i]]
         
-        return intValue
+        
