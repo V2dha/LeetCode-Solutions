364d21b3f9978e159dbf2c65b21da123e03e15b3
Vividha <rvividha@gmail.com>
2022-01-15 00:16:54 +0530

    Time: 123 ms (24.41%), Space: 21.7 MB (60.77%) - LeetHub

--- 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
+++ 124-binary-tree-maximum-path-sum/124-binary-tree-maximum-path-sum.py
@@ -7,12 +7,15 @@
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
         """
+        Approach - 
+        1. Maintain a global maxSum which will store the maximum cumulative sum so far
+        2. Recurse through left and right subTrees and update the maxSum i.e. left+right+root.val only if it is greater than maxSum
+        3. Return the maximum out of left children + root.val, right children + root.val or 0 in order to prevent adding negative numbers
         """
         self.maxSum = float("-inf")
         def recurseUtil(root):
             if not root:
                 return 0
-
             left = recurseUtil(root.left)
             right = recurseUtil(root.right)
             self.maxSum = max(self.maxSum, left+right+root.val)
