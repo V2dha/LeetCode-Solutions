4937475faf226288f1abf2acf60377166fcd1bca
Vividha <rvividha@gmail.com>
2022-01-14 23:23:51 +0530

    Time: 57 ms (30.84%), Space: 16.5 MB (53.49%) - LeetHub

--- 98-validate-binary-search-tree/98-validate-binary-search-tree.py
+++ 98-validate-binary-search-tree/98-validate-binary-search-tree.py
@@ -11,17 +11,32 @@ class Solution:
         Since inorder traversal traverses the BST in ascending order so it can be used to check the 
         validity of BST
         """
-        def inorder(root, elements):
-            if not root:
-                return 
-            inorder(root.left, elements)
-            elements.append(root.val)
-            inorder(root.right, elements)
+#         def inorder(root, elements):
+#             if not root:
+#                 return 
+#             inorder(root.left, elements)
+#             elements.append(root.val)
+#             inorder(root.right, elements)
             
-        elements = []
-        inorder(root, elements)
-        for i in range(1, len(elements)):
-            if elements[i-1] >= elements[i]:
-                return False
-        return True
+#         elements = []
+#         inorder(root, elements)
+#         for i in range(1, len(elements)):
+#             if elements[i-1] >= elements[i]:
+#                 return False
+#         return True
+        """
+        Second Approach - Recursive
+        """
+        self.answer=True
+        
+        def dfs(root, left, right):
+            if root:
+                if left >= root.val or root.val >= right:
+                    self.answer = False
+                    return
+                dfs(root.left, left, root.val)
+                dfs(root.right, root.val, right)
+                
+        dfs(root, float("-inf"), float('inf'))
+        return self.answer
         
