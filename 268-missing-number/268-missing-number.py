14f05e4c137af2ccf6be460bd13a02a16b201dd6
Vividha <rvividha@gmail.com>
2022-01-21 23:34:11 +0530

    Time: 4038 ms (5.00%), Space: 15.5 MB (51.64%) - LeetHub

--- 268-missing-number/268-missing-number.py
+++ 268-missing-number/268-missing-number.py
@@ -1,6 +1,6 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        nums.sort()
+       
         for i in range(len(nums)):
             if i not in nums:
                 return i
