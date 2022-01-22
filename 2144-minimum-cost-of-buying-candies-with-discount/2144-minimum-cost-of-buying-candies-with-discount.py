3ce496acb372b20db66125a9fffb690e5c95b524
Vividha <rvividha@gmail.com>
2022-01-22 21:46:37 +0530

    Time: 103 ms (57.14%), Space: 14.3 MB (14.29%) - LeetHub

--- 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.py
+++ 2144-minimum-cost-of-buying-candies-with-discount/2144-minimum-cost-of-buying-candies-with-discount.py
@@ -1,4 +1,9 @@
 class Solution:
+    """
+Approach -
+Sort the array in descending order and add the first two items after ordering them into group of 3 iteratively
+Return the minCost if it is a multiple of 3 else if it is not then add the cost of the remaining items
+    """
     def minimumCost(self, cost: List[int]) -> int:
         n = len(cost)
         if n == 1 or n == 2:
