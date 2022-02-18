6cff6264ebb10078930ee3730cc83adf17b71a24
Vividha <rvividha@gmail.com>
2022-02-19 02:01:25 +0530

    Time: 60 ms (49.42%), Space: 14.2 MB (80.65%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.py
+++ 402-remove-k-digits/402-remove-k-digits.py
@@ -1,22 +1,25 @@
 class Solution:
     def removeKdigits(self, num: str, k: int) -> str:
+        """
+        Approach - 1. Push into stack if the number on top of the stack is greater than the current number
+        Time Complexity = O(N)
+        Space Complexity = O(N)
+        """    
         stack = []
         for n in num:
-            while( stack and int(stack[-1]) > int(n) and k):
+            while stack and int(stack[-1]) > int(n) and k:
                 stack.pop()
-                k -= 1
+                k-=1
             stack.append(str(n))
-        
-        # If no elements are removed, pop last elements, (increasing order)
-        while(k):
-            stack.pop()
-            k -= 1
 
-        # removing leading zeros
-        i = 0
-        while( i <len(stack) and stack[i] == "0" ):
-            i += 1
-            
-        return ''.join(stack[i:]) if (len(stack[i:]) > 0) else "0"       
-        
+        while k:
+            stack.pop()
+            k-=1
+
+        i=0
+        while i < len(stack) and stack[i] == "0":
+            i+=1
+
+        return "".join(stack[i:]) if len(stack[i:]) > 0 else "0"
+
         
