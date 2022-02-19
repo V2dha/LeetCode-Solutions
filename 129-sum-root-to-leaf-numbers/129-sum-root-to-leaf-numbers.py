6322a8ed3d5d185ca080668ff65e02e4ccc68691
Vividha <rvividha@gmail.com>
2022-02-19 22:34:36 +0530

    Time: 24 ms (98.13%), Space: 13.9 MB (90.28%) - LeetHub

--- 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
+++ 129-sum-root-to-leaf-numbers/129-sum-root-to-leaf-numbers.py
@@ -7,23 +7,40 @@
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
         """
-        Approach : DFS with Stack
-        Time Complexity = O(N) 
-        Space Complexity = O(N)
+        Approach - Recursion DFS
+        Time Complexity and Space Complexity = O(N)
         """
+        self.res = 0
+        self.dfs(root, 0)
+        return self.res
+    
+    def dfs(self, root, path):
         if not root:
             return 0
-        stack = []
-        stack.append(root)
-        res = 0
-        while stack:
-            node = stack.pop()
-            if not node.left and not node.right:
-                res += node.val
-            if node.right:
-                node.right.val += node.val*10
-                stack.append(node.right)
-            if node.left:
-                node.left.val += node.val*10
-                stack.append(node.left)
-        return res
+        if not root.left and not root.right:
+            path = path*10 + root.val
+            self.res += path
+        self.dfs(root.left, path*10+root.val)
+        self.dfs(root.right, path*10+root.val)
+        
+        # """
+        # Approach : DFS with Stack
+        # Time Complexity = O(N) 
+        # Space Complexity = O(N)
+        # """
+        # if not root:
+        #     return 0
+        # stack = []
+        # stack.append(root)
+        # res = 0
+        # while stack:
+        #     node = stack.pop()
+        #     if not node.left and not node.right:
+        #         res += node.val
+        #     if node.right:
+        #         node.right.val += node.val*10
+        #         stack.append(node.right)
+        #     if node.left:
+        #         node.left.val += node.val*10
+        #         stack.append(node.left)
+        # return res
