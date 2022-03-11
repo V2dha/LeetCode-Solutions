23ed4e1f8e299a6e715f2683c7f087633232ea4d
Vividha <rvividha@gmail.com>
2022-03-11 15:56:15 +0530

    Time: 1643 ms (33.37%), Space: 25.1 MB (21.25%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -1,5 +1,11 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        """
+        Approach - Find the local minimum and get the maximum profit
+        Iterate through the array and update the minimum price and maximum profit
+        Time Complexity - O(N)
+        Space Complexity - O(1)
+        """
         profit = 0
         profitsoFar = 0
         lmin = prices[0]
