2acf12d8ff0411d09a349c4eb6a58b39d17b27b6
Vividha <rvividha@gmail.com>
2022-03-11 15:32:15 +0530

    Time: 760 ms (87.06%), Space: 28.6 MB (29.98%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.py
+++ 53-maximum-subarray/53-maximum-subarray.py
@@ -1,5 +1,13 @@
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        """
+        Approach - Kadane's Algorithm
+        1. initialise current sum and overall sum to first value 
+        2. iterate from 1 to end, if csum >= 0 add nums[i] else initialise csum = nums[i]
+        3. update maximum of osum and csum
+        Time Complexity - O(N)
+        Space Complexity - O(1)
+        """
         csum = nums[0]
         osum = nums[0]
         for i in range(1, len(nums)):
