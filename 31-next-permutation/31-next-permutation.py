f1883bcafed485c5d68ddf602c38c6995aff33df
Vividha <rvividha@gmail.com>
2022-03-11 15:12:52 +0530

    Time: 55 ms (61.55%), Space: 13.9 MB (85.89%) - LeetHub

--- 31-next-permutation/31-next-permutation.py
+++ 31-next-permutation/31-next-permutation.py
@@ -3,21 +3,23 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        """
+        Time Complexity - O(N) (3*O(N))
+        Space Complexity - O(1)
+        """
         n = len(nums)
-        k = -1
-        for i in range(n-2, -1, -1):
+        for i in range(n-2, -2, -1):
             if nums[i] < nums[i+1]:
-                k = i
                 break
-        if k < 0:
+        if i < 0:
             nums.reverse()
             return 
         else:
-            for j in range(n-1,  k, -1):
-                if nums[j] > nums[k]:
+            for j in range(n-1,  i, -1):
+                if nums[j] > nums[i]:
                     break
-            nums[j], nums[k] = nums[k], nums[j]
-            l, r = k+1, len(nums)-1  # reverse the second part
+            nums[j], nums[i] = nums[i], nums[j]
+            l, r = i+1, len(nums)-1  # reverse the second part
             while l < r:
                 nums[l], nums[r] = nums[r], nums[l]
                 l +=1 ; r -= 1
