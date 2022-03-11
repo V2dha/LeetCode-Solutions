2b614271ebe87fb12e6c0022bcf2c079b2cd0ba4
Vividha <rvividha@gmail.com>
2022-03-11 23:35:24 +0530

    Time: 40 ms (84.61%), Space: 13.9 MB (54.84%) - LeetHub

--- 61-rotate-list/61-rotate-list.py
+++ 61-rotate-list/61-rotate-list.py
@@ -11,7 +11,7 @@ class Solution:
         3. Store head in temp and iterate till count-k-1
         4. Set temp.next as result and temp.next as None and return result
         Time Complexity - O(N)
-        Space Complexity - 1
+        Space Complexity - O(1)
         """
         if not head:
             return None
@@ -22,11 +22,21 @@ class Solution:
         while itr.next:
             itr = itr.next
             count += 1
+            
         k = k % count
+        
+        # Set the last node to point to head node
+        # The list is now a circular linked list with last node pointing to first node
         itr.next = head
+        
         temp = head
+        
+        # Traverse the list to get to the node just before the ( length - k )th node.
+        # Example: In 1->2->3->4->5, and k = 2
+        #          we need to get to the Node(3)
         for _ in range(count-k-1):
             temp = temp.next
+            
         result = temp.next
         temp.next = None
         return result
