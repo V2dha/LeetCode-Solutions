50667d44d96a78fa80cba6a487d7d1b9d52a7c2b
Vividha <rvividha@gmail.com>
2022-03-13 22:27:52 +0530

    Time: 226 ms (52.57%), Space: 15.5 MB (31.79%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -20,22 +20,22 @@ class Solution:
                 count -= 1
         return ele
     
-        # """
-        # Approach - Store key value pairs of nums[i] in hmap
-        # if the value > n//2 then return key
-        # Time Complexity - O(N)
-        # Space Complexity - O(N)
-        # """
-        # if len(nums) == 1:
-        #     return nums[0]
-        # hmap = {}
-        # for num in nums:
-        #     if num in hmap:
-        #         hmap[num] += 1
-        #         if hmap[num] > len(nums)//2:
-        #             return num
-        #     else:
-        #         hmap[num] = 1
+        """
+        Approach - Store key value pairs of nums[i] in hmap
+        if the value > n//2 then return key
+        Time Complexity - O(N)
+        Space Complexity - O(N)
+        """
+        if len(nums) == 1:
+            return nums[0]
+        hmap = {}
+        for num in nums:
+            if num in hmap:
+                hmap[num] += 1
+                if hmap[num] > len(nums)//2:
+                    return num
+            else:
+                hmap[num] = 1
         
         # ans = sorted(hmap.items(), key=lambda x : x[1])
         # return ans[-1][0]
