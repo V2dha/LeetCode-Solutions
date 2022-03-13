b705b40c108fdd63948eea58af4824001202cba8
Vividha <rvividha@gmail.com>
2022-03-13 22:14:53 +0530

    Time: 235 ms (47.80%), Space: 15.5 MB (78.92%) - LeetHub

--- 169-majority-element/169-majority-element.py
+++ 169-majority-element/169-majority-element.py
@@ -1,13 +1,16 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
         hmap = {}
-        
         for num in nums:
             if num in hmap:
                 hmap[num] += 1
+                if hmap[num] > len(nums)//2:
+                    return num
             else:
                 hmap[num] = 1
         
-        ans = sorted(hmap.items(), key=lambda x : x[1])
-        return ans[-1][0]
+        # ans = sorted(hmap.items(), key=lambda x : x[1])
+        # return ans[-1][0]
         
