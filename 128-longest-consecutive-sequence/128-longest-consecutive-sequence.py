f313d13382a62fdccf5fed85ed68e3b6b3f8cb11
Vividha <rvividha@gmail.com>
2022-03-15 21:18:14 +0530

    Time: 403 ms (41.43%), Space: 27.6 MB (48.24%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -1,5 +1,15 @@
 class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
+        """
+        Approach - Using set
+        1. First turn the input into a set of numbers. That takes O(n) and O(1) for searching
+        2. If the number x is the start of a streak (i.e., x-1 is not in the set), 
+        then test y = x+1, x+2, x+3, ... and stop at the first number y not in the set
+        3. The length of the streak is then simply y-x and we update our global best with that. 
+        Since we check each streak only once, this is overall O(n)
+        Time Complexity - O(N)
+        Space Complexity - O(N)
+        """
         nums = set(nums)
         maxLen = 0
         for num in nums:
