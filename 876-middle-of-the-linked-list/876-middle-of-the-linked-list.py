de1d6ac6871d198fbe32c50ed7170d2d607a9340
Vividha <rvividha@gmail.com>
2022-03-17 19:21:02 +0530

    Time: 24 ms (98.79%), Space: 14 MB (19.93%) - LeetHub

--- 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.py
@@ -6,10 +6,22 @@
 class Solution:
     def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
         """
+        Approach 2 - Use slow and fast pointer (Similar to detect loop in LL)
+        
+        """
+        slow = head
+        fast = head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+        
+       
+        """
         Approach 1 - Iterate through ll once and count total nodes
         2. Iterate again and break when midCount == count//2 
         3. Return the midItr
-        Time Complexity - O(N)
+        Time Complexity - O(N) + O(N/2) (Two Traversals)
         Space Complexity - 1
         """
         if not head:
