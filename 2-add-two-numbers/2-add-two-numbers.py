48fe622906a2600759c5dceeb158320216b70af9
Vividha <rvividha@gmail.com>
2022-03-18 12:47:07 +0530

    Time: 77 ms (74.96%), Space: 14 MB (56.19%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.py
+++ 2-add-two-numbers/2-add-two-numbers.py
@@ -5,6 +5,34 @@
 #         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        """
+        Approach -
+        1.
+        Time Complexity - O(max(N, M))
+        Space Complexity - O(N)
+        """
+        start = ListNode(0)
+        temp = start
+        carry = 0
+        while l1 or l2 or carry:
+            csum = 0
+            if l1:
+                csum += l1.val
+                l1 = l1.next
+            if l2:
+                csum += l2.val
+                l2 = l2.next
+            csum += carry
+            carry = csum//10
+            node = ListNode(csum%10)
+            temp.next = node
+            temp = temp.next
+            
+        return start.next
+            
+        """
+        Approach 1 : Using lists
+        """
         A = ''
         B = ''
         while l1:
