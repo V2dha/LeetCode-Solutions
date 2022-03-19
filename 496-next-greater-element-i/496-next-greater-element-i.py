2dfbb33146340710c56c53a879f687465ff40ce7
Vividha <rvividha@gmail.com>
2022-03-19 15:42:44 +0530

    Time: 69 ms (61.20%), Space: 14.2 MB (66.70%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.py
+++ 496-next-greater-element-i/496-next-greater-element-i.py
@@ -1,8 +1,29 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
         """
+        Approach 2. Using hmap and stack
+        Time Complexity - O(N+M)
+        """
+        stack = []
+        hmap = {}
+        res = []
+        for i in range(len(nums2)-1, -1, -1):
+            while stack and nums2[i] > stack[-1]:
+                stack.pop()
+            if stack:
+                hmap[nums2[i]] = stack[-1]
+            stack.append(nums2[i])
+        
+        for num in nums1:
+            if num in hmap:
+                res.append(hmap[num])
+            else:
+                res.append(-1)
+        return res
+        
+        """
         Approach 1. Brute Force
-        Time Complexity - O(N*N)
+        Time Complexity - O(N*M)
         """
         res = []
         for i in range(len(nums1)):
