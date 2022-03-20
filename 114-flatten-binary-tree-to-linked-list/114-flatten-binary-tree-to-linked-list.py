84b490df917f4ac4da2f66f93726a8e277f05ea8
Vividha <rvividha@gmail.com>
2022-03-20 16:27:55 +0530

    Time: 302 ms (5.15%), Space: 15.3 MB (49.38%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.py
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.py
@@ -8,8 +8,8 @@ class Solution:
     prev = None
     def flatten(self, root: Optional[TreeNode]) -> None:
         """
-        Do not return anything, modify root in-place instead.
-        
+        Approach 1. Using Recursion 
+        Time Complexity - O(N) Space Complexity - O(N)
         """
         if not root:
             return 
@@ -23,28 +23,25 @@ class Solution:
         #Similar to iterative preorder traversal
         #in which stack can be used to append the root, root.right, and root.left keeping
         #track of the prev node
-        
+        """
+        Approach 2. Using stack
+        Time Complexity - O(N) Space Complexity - O(N)
+        """
         if not root:
-            return 
-        
+            return  
         rootStack = []
         prev = None
         rootStack.append(root)
-        
         while rootStack:
-            
             node = rootStack.pop()
-            
             #append right child first so that left child is traversed first
             if node.right:
                 rootStack.append(node.right)
             if node.left:
                 rootStack.append(node.left)
-                
             if prev is not None:
                 prev.right = node
                 prev.left = None
-                
             prev = node
  
             
