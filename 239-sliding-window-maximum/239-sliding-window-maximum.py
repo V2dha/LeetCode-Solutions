140279978a56d06fc915cd63b66caa2bef9920c3
Vividha <rvividha@gmail.com>
2022-03-20 12:15:50 +0530

    Time: 2287 ms (60.92%), Space: 30.2 MB (52.95%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.py
+++ 239-sliding-window-maximum/239-sliding-window-maximum.py
@@ -6,7 +6,7 @@ class Solution:
         qi = deque()
         
         for i in range(k):
-            while qi and nums[i] >= nums[qi[-1]]:
+            while qi and nums[i] > nums[qi[-1]]:
                 qi.pop()
             qi.append(i)
             
@@ -17,11 +17,24 @@ class Solution:
             while qi and qi[0] <= i-k:
                 qi.popleft()
             
-            while qi and nums[i] >= nums[qi[-1]]:
+            while qi and nums[i] > nums[qi[-1]]:
                 qi.pop()
                 
             qi.append(i)
         res.append(nums[qi[0]])
+        return res
+    
+        """
+        Approach 1. Brute Force
+        Time Complexity - O(N*K)
+        Space Complexity - O(1)
+        """
+        res = []
+        for i in range(len(nums)-k+1):
+            maxi = nums[i]
+            for j in range(i, i+k):
+                maxi = max(maxi, nums[j])
+            res.append(maxi)
         return res
         
         
