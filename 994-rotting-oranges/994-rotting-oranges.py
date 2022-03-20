7eb4334514a2f58664976a6d7cff992e674ee681
Vividha <rvividha@gmail.com>
2022-03-20 13:11:35 +0530

    Time: 68 ms (61.94%), Space: 14 MB (58.50%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -7,23 +7,23 @@ class Solution:
         """
         if not grid:
             return 0
-        m = len(grid)
+        m = len(grid)   
         n = len(grid[0])
-        minutes = 0
-        total = 0
-        count = 0
-        rotten = []
+        minutes = 0   #to store time
+        total = 0     #total number of fresh oranges
+        count = 0     #total number of oranges that can be rotten
+        rotten = []   #Queue to store position of rottened oranges
         for i in range(m):
             for j in range(n):
-                if grid[i][j] != 0:
+                if grid[i][j] != 0: #count fresh oranges
                     total += 1
-                if grid[i][j] == 2:
+                if grid[i][j] == 2: #store position of rotten oranges
                     rotten.append([i, j])
-        dx = [0,0,1,-1]
+        dx = [0,0,1,-1]  #directions
         dy = [1,-1,0,0]
-        while rotten:
-            k = len(rotten)
-            count += k
+        while rotten:    #queue is not empty
+            k = len(rotten)  
+            count += k   #add the oranges that have been rottened
             while k:
                 x = rotten[0][0]
                 y = rotten[0][1]
@@ -31,12 +31,16 @@ class Solution:
                 for i in range(4):
                     nx = x+dx[i]
                     ny = y+dy[i]
-                    if nx <0 or ny<0 or nx>=m or ny>=n or grid[nx][ny] !=1:
+                    #if index out of bounds or grid doesn't contain fresh oranges then continue
+                    if nx <0 or ny<0 or nx>=m or ny>=n or grid[nx][ny] !=1:  
                         continue
+                    #else mark it rotten and add the position to queue
                     grid[nx][ny] = 2
                     rotten.append([nx,ny])
                 k-=1
+            #count the total time of iteration of queue
             if rotten:
                 minutes += 1
+
         return minutes if total == count else -1
         
