057db0d7238b86211297ccbcb210aa03b2d108bf
Vividha <rvividha@gmail.com>
2022-03-21 13:14:46 +0530

    Time: 50 ms (81.43%), Space: 15.2 MB (84.39%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.py
@@ -2,13 +2,17 @@ class Solution:
     def frequencySort(self, s: str) -> str:
         hmap = {}
         res = ""
+        #O(N)
         for ch in s:
             if ch in hmap:
                 hmap[ch] += 1
             else:
                 hmap[ch] = 1
-        
+        #O(NlogN)
         hmap = sorted(hmap.items(), key = lambda x:x[1], reverse=True)
+        #O(N)
+        return "".join(map(lambda x:x[0]*x[1], hmap))
+    
         for key, value in hmap:
             res += key*value
         return res
