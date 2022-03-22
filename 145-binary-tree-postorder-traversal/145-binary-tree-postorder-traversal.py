511372969741677537a359e61bc410fcd61087d3
Vividha <rvividha@gmail.com>
2022-03-22 16:01:57 +0530

    Time: 40 ms (61.09%), Space: 13.8 MB (68.53%) - LeetHub

--- 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.py
+++ 145-binary-tree-postorder-traversal/145-binary-tree-postorder-traversal.py
@@ -7,10 +7,34 @@
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
         """
+        Approach 2. Using stack 
+        """
+        res = []
+        stack = []
+        if not root:
+            return res
+        node = root
+        while True:
+            if node:
+                res.append(node.val)
+                stack.append(node)
+                node = node.right
+            else:
+                if not stack:
+                    break
+                node = stack.pop()
+                node = node.left
+        return res[::-1]
+            
+        
+        
+        """
         Approach 1.Using Recursion
         1. Left subtree
         2. Right subtree
         3. Root node
+        Time Complexity - O(N)
+        Space Complexity - O(N) (Recursion Stack)
         """
         ele = []
         def postOrder(root, ele):
