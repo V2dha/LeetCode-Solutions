e28c9b7f15b6422959c3efcf4625e304c46e8d66
Vividha <rvividha@gmail.com>
2022-03-23 10:48:58 +0530

    Time: 1587 ms (43.75%), Space: 13.8 MB (93.87%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,12 +1,17 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        """"""
+        """
+        Approach - Centre Expansion 
+        Time Complexity - O(N*N)
+        Space Complexity - O(1)
+        """
         if s == None or len(s) < 1:
             return ""
         res_len = 0
         res_left = 0
         res_right = 0
         for i in range(len(s)):
+            #for cases like "racecar"
             l, r = i, i
             while l >= 0 and r < len(s) and s[l] == s[r]:
                 if r-l+1 > res_len:
@@ -15,6 +20,7 @@ class Solution:
                     res_len = r-l+1
                 l -= 1
                 r += 1
+            #for cases like "abba"
             l, r = i, i+1
             while l >= 0 and r < len(s) and s[l] == s[r]:
                 if r-l+1 > res_len:
