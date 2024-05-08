b6caf14d7e988e3c42252451fb53344559748324
Vividha <rvividha@gmail.com>
2024-05-08 19:47:52 +0530

    Time: 1372 ms (74.35%) | Memory: 0B (100.00%) - LeetSync

--- 1390-average-selling-price/average-selling-price.sql
+++ 1390-average-selling-price/average-selling-price.sql
@@ -1,15 +1,15 @@
 # Write your MySQL query statement below
 select p.product_id, 
-round(sum(u.units*p.price)/sum(u.units),2) as average_price
+coalesce(round(sum(u.units*p.price)/sum(u.units),2), 0) as average_price
 from Prices p
 left join UnitsSold u 
 on p.product_id = u.product_id 
-where u.purchase_date between p.start_date and
+and u.purchase_date between p.start_date and
 p.end_date
 group by p.product_id
-union
-select product_id, 0 as average_price
-from Prices
-where product_id not in 
-(select product_id from UnitsSold)
+-- union
+-- select product_id, 0 as average_price
+-- from Prices
+-- where product_id not in 
+-- (select product_id from UnitsSold)
 
