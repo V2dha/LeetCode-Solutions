bc0cb7e305a5454da1fb46dfd9cc32bd6aabe780
Vividha <rvividha@gmail.com>
2024-05-08 22:40:51 +0530

    Time: 909 ms (21.70%) | Memory: 0B (100.00%) - LeetSync

--- 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
+++ 1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -2,13 +2,14 @@
 
 select a.query_name,
 round(sum(a.rating/a.position)/count(*), 2) as quality,
-coalesce(round((b.poor_total/count(*))*100,2),0)
+coalesce(round(
+(sum(case when rating < 3 then 1 else 0 end)/count(*))*100,2),0)
 as poor_query_percentage
 from 
 (select * from Queries where query_name is 
 not null) a
-left join 
-(select query_name, count(*) as poor_total from Queries
-where rating < 3 group by query_name) b
-on a.query_name = b.query_name
+-- left join 
+-- (select query_name, count(*) as poor_total from Queries
+-- where rating < 3 group by query_name) b
+-- on a.query_name = b.query_name
 group by a.query_name
