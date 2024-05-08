f472e1b679f28b4d74e766ed0199673d9f5b08ff
Vividha <rvividha@gmail.com>
2024-05-08 12:58:42 +0530

    Time: 1223 ms (69.14%) | Memory: 0B (100.00%) - LeetSync

--- 2087-confirmation-rate/confirmation-rate.sql
+++ 2087-confirmation-rate/confirmation-rate.sql
@@ -1,18 +1,20 @@
 # Write your MySQL query statement below
-select a.user_id, 
+select c.user_id, 
 coalesce(round(b.confirmed/a.total, 2), 0) as 
-confirmation_rate from
+confirmation_rate from Signups c
+left join
 (select user_id, count(*) as total
 from Confirmations
 group by user_id) a
+on a.user_id = c.user_id
 left join 
 (select user_id, count(*) as confirmed
 from Confirmations
 where action = 'confirmed'
 group by user_id) b
 on a.user_id = b.user_id
-union
-select user_id, 0 as confirmation_rate from
-Signups where user_id not in 
-(select user_id from Confirmations)
+-- union
+-- select user_id, 0 as confirmation_rate from
+-- Signups where user_id not in 
+-- (select user_id from Confirmations)
 
