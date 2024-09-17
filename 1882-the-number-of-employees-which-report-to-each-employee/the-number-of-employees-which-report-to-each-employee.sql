4e9ad86da0fc0ba4f61fc9407ecac4539a2b05a2
Vividha <rvividha@gmail.com>
2024-09-17 20:57:35 +0530

    Time: 867 ms (33.71%) | Memory: 0B (100.00%) - LeetSync

--- 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
+++ 1882-the-number-of-employees-which-report-to-each-employee/the-number-of-employees-which-report-to-each-employee.sql
@@ -1,12 +1,21 @@
 /* Write your PL/SQL query statement below */
-with cte as 
+-- with cte as 
+-- (select reports_to, count(employee_id) as reports_count, 
+-- round(sum(age)/count(employee_id)) as average_age 
+-- from employees
+-- where reports_to is not null
+-- group by reports_to) 
+-- select a.employee_id, a.name, c.reports_count, c.average_age from 
+-- cte c inner join employees a
+-- on c.reports_to = a.employee_id
+-- order by a.employee_id;
+
+select a.employee_id, a.name, c.reports_count, c.average_age from
 (select reports_to, count(employee_id) as reports_count, 
 round(sum(age)/count(employee_id)) as average_age 
 from employees
 where reports_to is not null
-group by reports_to) 
-select a.employee_id, a.name, c.reports_count, c.average_age from 
-cte c inner join employees a
+group by reports_to) c 
+inner join employees a
 on c.reports_to = a.employee_id
-order by a.employee_id
-;
+order by a.employee_id;
