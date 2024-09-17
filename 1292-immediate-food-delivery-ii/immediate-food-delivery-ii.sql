cf4bb9c81eaa659b17662b11bb5de90de5c95722
Vividha <rvividha@gmail.com>
2024-09-17 23:28:28 +0530

    Time: 687 ms (84.46%) | Memory: 0B (100.00%) - LeetSync

--- 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
+++ 1292-immediate-food-delivery-ii/immediate-food-delivery-ii.sql
@@ -1,13 +1,22 @@
 /* Write your PL/SQL query statement below */
-with cte as 
-(select customer_id, min(order_date) as first_order_date
+-- with cte as 
+-- (select customer_id, min(order_date) as first_order_date
+-- from delivery
+-- group by customer_id)
+-- select 
+-- round(sum(case 
+-- when d.customer_pref_delivery_date=d.order_date 
+-- then 1 else 0 end)*100/count(*),2) as immediate_percentage
+-- from cte c
+-- join delivery d
+-- on c.customer_id = d.customer_id and c.first_order_date = d.order_date
+-- ;
+
+with cte as (
+select  
+rank() over (partition by customer_id order by order_date) as order_rnk,
+case when customer_pref_delivery_date=order_date then 1 else 0 end order_int_count
 from delivery
-group by customer_id)
-select 
-round(sum(case 
-when d.customer_pref_delivery_date=d.order_date 
-then 1 else 0 end)*100/count(*),2) as immediate_percentage
-from cte c
-join delivery d
-on c.customer_id = d.customer_id and c.first_order_date = d.order_date
-;
+)
+select round(sum(order_int_count)*100/count(*),2) as immediate_percentage from cte 
+where order_rnk = 1;
