aeee8107c3ca27b18bb62a07cbabb5025df32880
Vividha <rvividha@gmail.com>
2024-09-20 18:54:04 +0530

    Time: 650 ms (47.58%) | Memory: 0B (100.00%) - LeetSync

--- 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
+++ 1135-customers-who-bought-all-products/customers-who-bought-all-products.sql
@@ -1,9 +1,7 @@
 /* Write your PL/SQL query statement below */
 
-with cte as
-(select count(*) as total_prd from product)
 select customer_id
 from customer 
 group by customer_id
-having count(distinct product_key) =
-(select total_prd from cte)
+having count(distinct product_key) = 
+(select count(*) as total_prd from product)
