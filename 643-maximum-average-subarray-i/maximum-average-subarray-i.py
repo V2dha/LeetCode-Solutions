48c435da9f9f341daa931363ff4e31af903b2cd4
Vividha <rvividha@gmail.com>
2025-03-30 21:30:31 +0530

    Time: 91 ms (43.07%) | Memory: 27.5 MB (39.76%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.py
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.py
@@ -5,7 +5,7 @@ class Solution:
         if len(nums) == k:
             return sum(nums)/k
 
-
+        # O n*k
         # for i in range(len(nums)-k+1):
         #     avg = sum(nums[i:i+k])/k
         #     max_avg = max(max_avg, avg)
@@ -13,11 +13,10 @@ class Solution:
         # return max_avg
 
         ind_sum = sum(nums[:k])
+        max_sum = ind_sum
 
         for i in range(1, len(nums)-k+1):
-            max_sum = max(max_sum, ind_sum)
             ind_sum = ind_sum - nums[i-1] + nums[i+k-1]
-
             max_sum = max(max_sum, ind_sum)
             
         return max_sum/k
